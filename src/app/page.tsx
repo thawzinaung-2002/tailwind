d80159377b12bba35e785b6b66b9ff9000d322d5
Thaw Zin Aung <tz.a@mit.com.mm>
2025-06-28 02:09:04 +0630

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World
-    </h1>
+    <div>
+      <h1 className="border-4 border-amber-300">
+        Hello World
+      </h1>
+      <h1 className="text-red-500">
+        Hello World
+      </h1>
+    </div>
   )
 }
